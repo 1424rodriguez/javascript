2ca690587471bcff1fbe5c70bd3e857792f77021
1424rodriguez <56600531+1424rodriguez@users.noreply.github.com>
2020-01-09 13:10:11 +0100

    Add files via upload

---
+++ TAREFA1/escripito.js
@@ -0,0 +1,34 @@
+/* 
+	Primeira tarefa
+	Nome do ficheiro: escripito.js
+	Usado: alert()
+*/
+
+
+
+alert("que tal?");
+alert("Moi boas a todos");
+
+
+
+/* Funciona copia e pega o que vai entre comiñas*/
+
+alert("2+2");
+
+/* Lo que ocurre es que lo copia y lo pega como los anteriores */
+
+alert(2+2);
+
+/* Lo que ocurre es , simplemente, que resuelve la operación matemática */
+
+alert(2.1+2.5);
+
+/* Operación con decimales, funciona correctamente */
+
+alert(50*50);
+
+/* Multiplicaciones */
+
+alert(1/3);
+
+/*Divisiones*/
