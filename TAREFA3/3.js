3a0015787c88986828d374ec44e0f4b5dbd1952b
1424rodriguez <56600531+1424rodriguez@users.noreply.github.com>
2020-01-14 19:36:52 +0100

    Add files via upload

---
+++ TAREFA3/3.js
@@ -0,0 +1,29 @@
+var moito = 9 ;
+var raiz = Math.sqrt(moito);
+alert(raiz);
+/*Ejemplo*/
+
+var aaa = 3.4234;
+var bbb = 6.4218;
+var ccc = 2.7684;
+var pi = Math.PI;
+
+var prueba1 = Math.sqrt(aaa);
+alert(prueba1);
+/*Raíz Cuadrada*/
+
+var prueba2 = Math.pow(bbb,2);
+alert(prueba2);
+/*Cuadrado*/
+
+var prueba3 = Math.round(pi);
+alert(prueba3);
+/*Número PI*/
+
+var prueba4 = Math.min(aaa,bbb,ccc);
+alert(prueba4);
+/*Funcion que te indica la menor de las variables*/
+
+var prueba5 = Math.random(0 ,3);
+alert(prueba5);
+/*Número aleatorio entre 0 y 3*/
