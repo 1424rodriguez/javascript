dedaaaceaec6671865059ffd99e0954401867bc8
1424rodriguez <56600531+1424rodriguez@users.noreply.github.com>
2020-01-16 09:14:12 +0100

    Add files via upload

---
+++ TAREFA5_ducias2ovos/escripito.js
@@ -0,0 +1,24 @@
+/* 
+	Primeira tarefa
+	Nome do ficheiro: escripito.js
+	Usado: alert()
+*/
+
+
+
+var ducias;
+/* Declaramos la variable*/
+var numducias;
+/*Declaramos otra variable*/
+ducias = prompt("Cantas ducias de ovos leva una tortilla");
+/*Preguntalle ao usuario a pregunta */
+numducias = parseInt(ducias);
+/*Convierte lo que el usuario a número*/
+alert(ducias);
+/*Muestra la alerta del usuario*/
+var ovos;
+/*Declaramos otra variable*/
+ovos = 12* numducias;
+/*Para saber cuantos huevos son el numero de ducias que introducimos al principio*/
+alert(ovos);
+/*Muestra la alerta el resultado de la cuenta anterior*/
